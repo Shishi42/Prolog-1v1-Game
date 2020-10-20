7dfc7123c8c5792220c29eaf765d91a15d3a632e
Shishi42 <benriv4272@gmail.com>
2020-10-20 11:24:31 +0200

    je puch

---
+++ yolo.pl
@@ -0,0 +1,19 @@
+succNum(1,2).
+succNum(2,3).
+succNum(3,4).
+succNum(4,5).
+succNum(5,6).
+succNum(6,7).
+succNum(7,8).
+
+succAlpha(a,b).
+succAlpha(b,c).
+succAlpha(c,d).
+succAlpha(d,e).
+succAlpha(e,f).
+succAlpha(f,g).
+succAlpha(g,h).
+
+afficheLigne([X|L]) :- write(X), write(' '), afficheLigne(L).
+
+afficheGrille()
